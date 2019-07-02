aedf820f1dd24d4178defd229eb3bb464bae5a8f
Serban QLV <serban@qluvio.com>
2019-07-01 23:31:17 -0700

    Alternative to aviobuf assert

--- libavformat/aviobuf.c
+++ libavformat/aviobuf.c
@@ -542,7 +542,16 @@ static void fill_buffer(AVIOContext *s)
 
             s->checksum_ptr = dst = s->buffer;
         }
-        len = ctx->orig_buffer_size;
+
+        // serban@eluv.io - test an alternative to assert
+        // av_assert0(len >= s->orig_buffer_size);
+        // len = s->orig_buffer_size;
+        if (len >= ctx->orig_buffer_size) {
+            len = ctx->orig_buffer_size;
+        } else {
+            av_log(s, AV_LOG_ERROR, "avio fill_buffer would assert len=%d orig_buffer_size=%d opaque=%p",
+                len, ctx->orig_buffer_size, s->opaque);
+        }
     }
 
     len = read_packet_wrapper(s, dst, len);
