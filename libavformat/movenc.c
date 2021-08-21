5b7fc1681a2c90ab0c3ebe12b249a9a58f79ee08
Reza mIr <reza@eluv.io>
2021-08-20 22:04:34 -0400

    Rollback the dts threshold completely

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -6471,13 +6471,12 @@ static int mov_auto_flush_fragment(AVFormatContext *s, int force)
     return ret;
 }
 
-#define DTS_THRESHOLD   1
+#define DTS_THRESHOLD   0
 
 static int check_pkt(AVFormatContext *s, MOVTrack *trk, AVPacket *pkt)
 {
     int64_t ref;
     uint64_t duration;
-    int64_t dts_with_threshold;
 
     if (trk->entry) {
         ref = trk->cluster[trk->entry - 1].dts;
@@ -6493,9 +6492,8 @@ static int check_pkt(AVFormatContext *s, MOVTrack *trk, AVPacket *pkt)
         ref -= trk->dts_shift;
     }
 
-    dts_with_threshold = pkt->dts + DTS_THRESHOLD;
-    duration = dts_with_threshold - ref;
-    if (dts_with_threshold < ref || duration >= INT_MAX) {
+    duration = pkt->dts - ref;
+    if (pkt->dts < ref || duration >= INT_MAX) {
         av_log(s, AV_LOG_ERROR, "Application provided duration: %"PRId64" / timestamp: %"PRId64" is out of range %"PRId64" for mov/mp4 format\n",
             duration, pkt->dts, ref
         );
